7b8678a308daa069bac9a89da464fe0b3262d036
Sujanshr <sujan191311@iimscollege.edu.np>
2020-10-04 10:38:35 +0545

    array and objects

---
+++ array/index.js
@@ -0,0 +1,47 @@
+// //let fruit = ['apple','ball','car','doll',1,true,null,[1,2]];
+// //let newArr = [['hello',2, true], ['iims','bscit'],['kathmandu','pokhara','dharan']];
+// //console.log(fruit[2]);
+// //fruit[2] = 'Avacado';
+// //console.log(fruit[2]);
+// //console.log(fruit.length);
+// //console.log(fruit.indexOf('doll'));
+// //fruit.push('taalu');
+// //fruit.pop();
+// //console.log(fruit);
+// // let fruity = new Array('alu','talu','kalu')
+// // console.log(fruity.pop());
+// // fruity.push('bandre');
+// // fruity.unshift('kukur');
+// // console.log(fruity);
+// // fruity[1] = 'makalu';
+// // console.log(fruity);
+// //console.log(newArr[2][2]);
+// //console.log(fruit[7]);
+// //console.log(fruit[7][0]);
+// //fruity.splice(2,1,'apple','ball');
+// //console.log(fruity);
+//
+// // add joker at index 2 without removing any items
+// let dc = ['superman', 'batman', ['flash','wonderwoman'], 'aquaman'];
+// dc[2].splice(0,0,'joker');
+// //dc[2].unshift('joker');
+// console.log(dc);
+
+
+//last push and concat
+// let dc = ['superman', 'batman', 'aquaman'];
+// dc.splice(3,0,'joker');
+// console.log(dc);
+// let villian = ['black adam'];
+// console.log(dc.concat(villian));
+
+// let dc = ['superman', 'batman', 'aquaman', 'flash','wonderwoman'];
+// let citrus = dc.slice(3);
+// console.log(citrus);
+
+let student = {};
+student.name = 'sir';
+student.id = 12345;
+student.contact = 9812345678;
+student.faculty = 'IT';
+console.log(student);
